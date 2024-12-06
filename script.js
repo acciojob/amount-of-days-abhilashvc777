d24ea11dae410adc3ce91bb5c71983b8e0de938a
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-06 19:22:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfYear(year){
+	if(year%400==0 && year%100==0){
+		return "366"
+	}else{
+		return "365"
+	}
+}
+const year = 1970
+const day = daysOfYear(year)
+console.log(day)
